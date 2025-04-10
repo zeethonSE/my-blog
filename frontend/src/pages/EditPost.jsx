8c2d030373a4300e2b3b8b07cd2d8c7762f25ca4
zeethonSE <zeethon0@gmail.com>
2025-04-10 21:50:03 +0700

    Fix CORS and API config

--- frontend/src/pages/EditPost.jsx
+++ frontend/src/pages/EditPost.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const PATH = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const PATH = import.meta.env.VITE_API_URL;
 function EditPost(){
 
     const {id} = useParams();
--- frontend/src/pages/NewPost.jsx
+++ frontend/src/pages/NewPost.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
 
-const PATH = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const PATH = import.meta.env.VITE_API_URL;
 function NewPost(){
 
     const [title, setTitle] = useState("");
