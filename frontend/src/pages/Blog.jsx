4969022bf72cdf4612908e0f68617af87e5d416f
zeethonSE <zeethon0@gmail.com>
2025-04-19 22:03:46 +0700

    update postdetail UI

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -14,12 +14,11 @@ function Blog() {
   }, []);
 
   return (
-    <div className="p-4 w-full">
+    <div className="p-4 h-100 w-full">
       <h1 className="text-2xl font-bold mb-4">Blog Posts</h1>
       {posts.map(post => (
         <Link key={post.id} to={`/post/${post.id}`} className="block p-4 h-auto rounded-md bg-linear-to-b from-black to-blue-500">
           <h2 className="text-xl font-semibold">{post.title}</h2>
-          <p>{post.description}</p>
         </Link>
       ))}
     </div>
--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -32,14 +32,14 @@ function PostDetails() {
   if (!post) return <h2>Loading...</h2>;
 
   return (
-    <div className="p-4 h-screen w-full md:w-2/3 rounded-md bg-cover bg-center bg-[url('./Travel-Blog.png')]">
-      <h2 className="text-2xl font-bold bg-orange-100">{post.title}</h2>
-      <p className="bg-orange-200">{post.content}</p>
+    <div className="p-4 h-screen w-full md:w-2/3 bg-cover bg-center bg-[url('/Travel-Blog.png')]">
+      <h2 className="p-4 text-2xl rounded-md font-bold bg-orange-200">{post.title}</h2>
+      <p className="p-4 h-300 scroll-auto rounded-md bg-orange-100">{post.content}</p>
       <div className="flex gap-4 mt-4 justify-items-center">
-        <Link to={`/edit/${id}`} className="text-blue-500 hover:text-green-500 p-2 rounded bg-orange-100">
+        <Link to={`/edit/${id}`} className="text-blue-500 hover:text-green-500 p-2 rounded bg-orange-200">
           Edit
         </Link>
-        <button onClick={handleDelete} className="text-blue-500 hover:text-green-500 p-2 rounded bg-orange-100">
+        <button onClick={handleDelete} className="text-blue-500 hover:text-green-500 p-2 rounded bg-orange-200">
           Delete
         </button>
       </div>
