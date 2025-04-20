87ec0d07413ca4d280621210ad8217ade03e2424
zeethonSE <zeethon0@gmail.com>
2025-04-20 21:02:50 +0700

    Update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), require('@tailwindcss/typography')],
+  plugins: [react(), tailwindcss()],
 })
