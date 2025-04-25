2f35549fc0503341c8f0d9f64a51d6c8090b7945
zeethonSE <zeethon0@gmail.com>
2025-04-25 17:01:16 +0700

    Update PostDetails

--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -34,9 +34,9 @@ function PostDetails() {
   console.log("Content:", post.content);
   const mark = '# Hello, *world*!';
   return (
-    <div className="p-4 mt-4 gap-4 h-screen rounded-md justify-items-center bg-center bg-[url('/Zeethon-post.png')]">
-      <h2 className="p-4 text-2xl rounded-md font-bold bg-green-300 border border-gray-500 w-full md:w-2/3">{post.title}</h2>
-      <div className="p-4 mt-4 rounded-md bg-green-300 border border-gray-500 w-full md:w-2/3">
+    <div className="p-4 mt-4 gap-4 h-screen rounded-md justify-items-center bg-cover bg-[url('/Zeethon-post.png')]">
+      <h2 className="p-4 text-2xl rounded-md font-bold bg-green-600 border border-gray-500 w-full md:w-2/3">{post.title}</h2>
+      <div className="p-4 mt-4 rounded-md bg-green-600 border border-gray-500 w-full md:w-2/3">
         <textarea 
           className="w-full font-semibold"
           value={post.content}
