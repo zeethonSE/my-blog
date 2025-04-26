fddb6993bb0ce59bdc93dbb163784b86158e36e0
zeethonSE <zeethon0@gmail.com>
2025-04-26 20:52:10 +0700

    Update Footer

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return(
         <footer>
-            <h5 className="text-xs text-gray-600 text-center">Copyright © {new Date().getFullYear()} Sai Sai Web Developer.</h5>
+            <h5 className="text-xs text-gray-600 text-center">Copyright © {new Date().getFullYear()} Sai Sai My Blog.</h5>
         </footer>
     )
 }
