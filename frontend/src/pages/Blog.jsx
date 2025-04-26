d95798a142b39bff220be06e881c2baac684b8f4
zeethonSE <zeethon0@gmail.com>
2025-04-26 15:43:27 +0700

    Update Blog

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -16,14 +16,13 @@ function Blog() {
   return (
     <div className="p-4 h-screen w-full bg-cover bg-center bg-[url('/renantaglia.avif')]">
       <h1 className="text-xl md:text-2xl font-bold mb-4">Blog Posts</h1>
-      <div className="p-4 h-auto rounded-md bg-linear-to-b from-black to-blue-500">
         {posts.map(post => (
-        <Link key={post.id} to={`/post/${post.id}`} className="block p-4 h-16 md:h-20 rounded-md bg-green-500">
-          <h2 className="text-l md:text-xl font-semibold">{post.title}</h2>
-        </Link>
-        ))}
-      </div>
-      
+          <div className="p-4 h-auto rounded-md bg-linear-to-b from-black to-blue-500">
+            <Link key={post.id} to={`/post/${post.id}`} className="block p-4 h-16 md:h-20 rounded-md bg-green-500">
+              <h2 className="text-l md:text-xl font-semibold">{post.title}</h2>
+            </Link>
+          </div>
+        ))} 
     </div>
   );
 }
